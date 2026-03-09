d8eff4fae9210bb55d975159bb15e9fb130642ba
thiagomenorpecinhamuitoquente <kava.thiago@escola.pr.gov.br>
2026-03-09 08:54:19 -0300

    Add files via upload

---
+++ aula1.js
@@ -0,0 +1,9 @@
+// cntrl + s
+// const -> variavel constante (nao pode ser alterada)
+// let -> variavel local
+// var -> variavel global
+let nome = "Thiago"
+let idade = "15"
+let altura = "1.76"
+let hobbie = "jogar video games"
+console.log("Olá, meu nome é " + nome + "! Tenho " + idade + " anos de idade! E " + "Gosto de " + hobbie) 
